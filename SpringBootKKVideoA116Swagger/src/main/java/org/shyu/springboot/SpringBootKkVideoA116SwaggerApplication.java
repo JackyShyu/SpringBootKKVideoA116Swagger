db02f6fcdc537cafb29e3137f1c6dd3011694e3e
jackyshyu <jackyshyu@gmail.com>
2018-10-23 17:58:58 -0500

    Create

---
+++ SpringBootKKVideoA116Swagger/src/main/java/org/shyu/springboot/SpringBootKkVideoA116SwaggerApplication.java
@@ -0,0 +1,12 @@
+package org.shyu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKkVideoA116SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKkVideoA116SwaggerApplication.class, args);
+	}
+}
